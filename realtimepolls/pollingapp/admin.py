63f70c83da2d9ecbc493dc20594ce67a8720c7d4
Mehmetcan KATIRCI <mcanktc06@gmail.com>
2025-08-12 09:52:33 +0300

    feat(django): configure models.py and admin.py

--- realtimepolls/pollingapp/admin.py
+++ realtimepolls/pollingapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Poll, PollOption, Vote
 # Register your models here.
+admin.site.register(Poll)
+admin.site.register(PollOption)
+admin.site.register(Vote)
+
--- realtimepolls/pollingapp/models.py
+++ realtimepolls/pollingapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+class PollOption(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name="options") 
+    text = models.CharField(max_length=240)
+
+class Vote(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    option = models.ForeignKey(PollOption, on_delete=models.CASCADE)
+    voter_key = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together =("poll", "voter_key")
